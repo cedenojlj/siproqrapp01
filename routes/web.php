f2abfc9d7c1fa154b29076af7482b35ca99c6ff7
cedenojlj <cedenojlj@gmail.com>
2025-08-08 12:44:38 -0400

    inicio con la vista login

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
